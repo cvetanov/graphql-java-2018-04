5b6d8bc8ebdd2d0f31f8b7413a313c1c591d687e
Goce Cvetanov <Goce.Cvetanov@netcetera.com>
2018-03-31 18:24:21 +0200

    setup graphql server - add maven dependencies - add initial schema and pojos

---
+++ server/src/main/java/com/gcv/graphql/config/BaseConfiguration.java
@@ -0,0 +1,29 @@
+package com.gcv.graphql.config;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import com.gcv.graphql.demo.Query;
+import graphql.schema.GraphQLSchema;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BaseConfiguration {
+
+    private final Query query;
+
+    @Autowired
+    public BaseConfiguration(Query query) {
+        this.query = query;
+    }
+
+    @Bean
+    public GraphQLSchema graphQLSchema() {
+
+        return SchemaParser.newParser()
+                .file("schema.graphqls")
+                .resolvers(query)
+                .build()
+                .makeExecutableSchema();
+    }
+}
---
+++ server/src/main/java/com/gcv/graphql/demo/POJO.java
@@ -0,0 +1,14 @@
+package com.gcv.graphql.demo;
+
+public class POJO {
+
+    private String hello;
+
+    public String getHello() {
+        return hello;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+}
